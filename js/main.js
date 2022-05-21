ba9f70e404a8db8b8462f10cdf6cd6a3720f008c
Nguyễn Lâm <lamnguyenit212@gmail.com>
2022-05-21 16:17:23 +0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,40 @@
+const $ = document.querySelector.bind(document)
+const $$ = document.querySelectorAll.bind(document)
+
+const tabs = $$('.tab-item')
+const panes = $$('.tab-pane')
+
+const tabActive = $('.tab-item.active')
+const line = $('.tabs .line')
+
+line.style.left = tabActive.offsetLeft + 'px'
+line.style.width = tabActive.offsetWidth + 'px'
+
+tabs.forEach((tab, index) => {
+    const pane = panes[index]
+    tab.onclick = function() {
+        $('.tab-item.active').classList.remove('active')
+        $('.tab-pane.active').classList.remove('active')
+        
+        line.style.left = this.offsetLeft + 'px'
+        line.style.width = this.offsetWidth + 'px'
+        this.classList.add('active')
+        pane.classList.add('active')
+    }
+})
+
+const gotopBtn = $('#gotop')
+window.onscroll = function() {scrollFunction()}
+
+gotopBtn.onclick = () => {
+    document.body.scrollTop = 0
+    document.documentElement.scrollTop = 0;
+}
+
+function scrollFunction() {
+    if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+        gotopBtn.style.display = "flex"
+    } else {
+        gotopBtn.style.display = "none";
+    }
+}
